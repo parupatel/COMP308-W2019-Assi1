518e3b92efe2304c6af44e5e074d4c5640931ae8
parupatel <connectparupatel@gmail.com>
2019-02-16 14:44:09 -0500

    final commit

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,21 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/about', (req, res, next) => {
-  res.render('index', { title: 'About' });
+  res.render('index', { title: 'About Me' });
+});
+
+router.get('/projects', (req, res, next) => {
+  res.render('../views/projects/index', { title: 'Projects' });
+});
+
+
+
+router.get('/services', (req, res, next) => {
+  res.render('../views/services/index', { title: 'Services' });
 });
 
 router.get('/contact', (req, res, next) => {
-  res.render('index', { title: 'Contact' });
-});
-
-router.get('/products', (req, res, next) => {
-  res.render('index', { title: 'Products' });
-});
-
-router.get('/services', (req, res, next) => {
-  res.render('index', { title: 'Services' });
+  res.render('index', { title: 'Contact Me' });
 });
 
 module.exports = router;
